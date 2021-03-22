d0543f52d4d7bd7d0dcf18a6248e4c1d4eed324c
Clark Winkelmann <clark.winkelmann@gmail.com>
2021-03-22 18:29:57 +0100

    Open modal on custom homepages as well

--- js/src/forum/openAutomaticModal.js
+++ js/src/forum/openAutomaticModal.js
@@ -4,6 +4,8 @@ import Page from 'flarum/components/Page';
 import IndexPage from 'flarum/components/IndexPage';
 import ChooseTagsToFollowModal from './components/ChooseTagsToFollowModal';
 
+/* global m */
+
 export default function () {
     let initialized = false;
 
@@ -15,7 +17,10 @@ export default function () {
         // We only show the modal if the first page loaded was the index page
         // And that new updates are available
         // And that the user *must* accept them
-        if (app.current.matches(IndexPage) && app.forum.attribute('clarkwinkelmannFollowTagsShouldPrompt')) {
+        if (
+            (app.current.matches(IndexPage) || m.route.get() === '/') &&
+            app.forum.attribute('clarkwinkelmannFollowTagsShouldPrompt')
+        ) {
             // This code is affected by the blue backdrop of death https://github.com/flarum/core/issues/1813
             // This can't be reliably reproduced, but happens mostly on Firefox
             // setTimeout doesn't solve the issue but brings a slight improvement, so we'll go with that for now
